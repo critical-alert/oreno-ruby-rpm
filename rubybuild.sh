1852a3895d7e1a646c9338be0e494a411db44814
Hirokazu Sugiuchi <critical.alert1@gmail.com>
2015-04-18 18:17:02 +0900

    Add rpm copy

--- rubybuild.sh
+++ rubybuild.sh
@@ -5,3 +5,5 @@ mkdir -p $HOME/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
 (cd $HOME/rpmbuild/SOURCES && curl -LO http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.2.tar.gz)
 
 rpmbuild -ba $HOME/rpmbuild/SPECS/ruby22x.spec
+
+cp $HOME/rpmbuild/RPMS/x86_64/* /shared/
